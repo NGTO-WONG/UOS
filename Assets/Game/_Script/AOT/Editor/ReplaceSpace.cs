fb86ad078879b5eed7e9b2c29c733d70a61db20a
qiang.huang <2012wong.q@gmail.com>
2023-10-03 17:57:14 +0800

    1

---
+++ Assets/Game/_Script/AOT/Editor/ReplaceSpace.cs
@@ -0,0 +1,71 @@
+using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Game._Script.AOT.Editor
+{
+    public class ReplaceSpace
+    {
+        [MenuItem("HybridCLR/Build/ReplaceSpace", priority = 401)]
+        public static void ReplaceSpacesWithUnderscores()
+        {
+            var path = Application.dataPath+"/Game/_Script/HotUpdate/Test";
+            Debug.Log(path);
+            ReplacePathSpace(path);
+        }
+
+        static void ReplacePathSpace(string path)
+        {
+            var directories = Directory.GetDirectories(path);
+            foreach (var subDirectory in directories)
+            {
+                var oldPath = path + "/" + Path.GetFileName(subDirectory);
+                var newName = Path.GetFileName(subDirectory).Replace(" ", "_");
+                var newPath = path+"/"+newName;
+                Directory.Move(oldPath,newPath);
+            }
+            
+            var files = Directory.GetFiles(path);
+            foreach (var file in files)
+            {
+                if (Path.GetFileName(file).Contains(".meta"))
+                {
+                    continue;
+                }
+                Debug.Log(Path.GetFileName(file));
+            }
+            
+        }
+        
+        
+        static void RemoveSpacesInFolderNames(string directory)
+        {
+            
+            try
+            {
+                // 处理当前目录下的所有文件夹
+                foreach (string subDirectory in Directory.GetDirectories(directory))
+                {
+                    string newDirectoryName = Path.GetFileName(subDirectory).Replace(" ", "_");
+                    string newDirectoryPath = Path.Combine(directory, newDirectoryName);
+                    Directory.Move(subDirectory, newDirectoryPath);
+                    //ReplaceSpacesWithUnderscores(newDirectoryPath); // 递归处理子文件夹
+                }
+
+                // 处理当前目录下的所有文件
+                foreach (string filePath in Directory.GetFiles(directory))
+                {
+                    string newFileName = Path.GetFileName(filePath).Replace(" ", "_");
+                    string newFilePath = Path.Combine(directory, newFileName);
+                    File.Move(filePath, newFilePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"删除文件夹名称中的空格时发生错误: {ex.Message}");
+            }
+        }
+    }
+}
