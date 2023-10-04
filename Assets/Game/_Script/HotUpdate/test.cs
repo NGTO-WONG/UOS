82adcb77d2b6e3d8a7268c8264418ba8afe09504
qiang.huang <2012wong.q@gmail.com>
2023-10-04 17:11:26 +0800

    1

---
+++ Assets/Game/_Script/HotUpdate/test.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        UnityEngine.Debug.Log(other.gameObject.name);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
