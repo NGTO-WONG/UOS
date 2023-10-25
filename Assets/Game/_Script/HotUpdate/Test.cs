7e28adc58e939f75c1d456c1b4a42e7122b0199e
huangqiang <2012wong.q@gmail.com>
2023-10-26 01:35:18 +0800

    1

--- Assets/Game/_Script/HotUpdate/Test.cs
+++ Assets/Game/_Script/HotUpdate/Test.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Spine.Unity;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    [SerializeField] private SkeletonAnimation skeletonAnimation; 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(skeletonAnimation.AnimationName);
     }
 }
