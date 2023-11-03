29123212b35d7aa44c6450e050830765695a659a
huangqiang <2012wong.q@gmail.com>
2023-11-03 12:25:18 +0800

    1

--- Assets/Game/_Script/AOT/Editor/BuildTool.cs
+++ Assets/Game/_Script/AOT/Editor/BuildTool.cs
@@ -102,10 +102,27 @@ namespace Game._Script.AOT.Editor
 
             Debug.Log("打包log：" + "Build finished!");
         }
+        
+        public static void GenerateAll(BuildTarget target)
+        {
+            CompileDllCommand.CompileDll(target);
+            Il2CppDefGeneratorCommand.GenerateIl2CppDef();
+
+            // 这几个生成依赖HotUpdateDlls
+            LinkGeneratorCommand.GenerateLinkXml(target);
+
+            // 生成裁剪后的aot dll
+            StripAOTDllCommand.GenerateStripedAOTDlls(target);
+
+            // 桥接函数生成依赖于AOT dll，必须保证已经build过，生成AOT dll
+            MethodBridgeGeneratorCommand.GenerateMethodBridge(target);
+            ReversePInvokeWrapperGeneratorCommand.GenerateReversePInvokeWrapper(target);
+            AOTReferenceGeneratorCommand.GenerateAOTGenericReference(target);
+        }
 
         public static void BuildAndCopyAndRenameDll(BuildTarget buildTarget)
         {
-            PrebuildCommand.GenerateAll();
+            GenerateAll(buildTarget);
             //生成linkFile
             var xmlPath = Application.dataPath + "/HybridCLRData/Generated/link.xml";
             BuildLinkFile.GenerateLinkfile(xmlPath);
