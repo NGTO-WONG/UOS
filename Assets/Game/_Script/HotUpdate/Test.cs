2ba59aa2a2d2ed882497092b5282faf1dbc614f1
huangqiang <2012wong.q@gmail.com>
2023-11-06 00:28:12 +0800

    1

--- Assets/Game/_Script/HotUpdate/Test.cs
+++ Assets/Game/_Script/HotUpdate/Test.cs
@@ -1,8 +1,15 @@
+using System;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
     void Start()
+    {
+        Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 0;
+    }
+
+    private void Update()
     {
         Application.targetFrameRate = 60;
         QualitySettings.vSyncCount = 0;
