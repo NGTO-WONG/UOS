b366f541ef6da7c71e62c0ebd165b5f0fea85515
huangqiang <2012wong.q@gmail.com>
2023-11-06 14:27:20 +0800

    1

--- Assets/Game/_Script/HotUpdate/Test.cs
+++ Assets/Game/_Script/HotUpdate/Test.cs
@@ -1,15 +1,25 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
+using YooAsset;
 
 public class Test : MonoBehaviour
 {
+    [SerializeField] private Button button;
     void Start()
     {
+        button.onClick.AddListener(() =>
+        {
+            var package = YooAssets.GetPackage("DefaultPackage");
+            package.UnloadUnusedAssets();
+        });
         Application.targetFrameRate = 300;
         QualitySettings.vSyncCount = 0;
         Debug.Log(Application.targetFrameRate);
         Debug.Log(Application.targetFrameRate);
         Debug.Log(Application.targetFrameRate);
     }
+        
+    
 
 }
