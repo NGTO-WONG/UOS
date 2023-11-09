ee3b1a6b40ecfb117da017940365405607acdbcb
huangqiang <2012wong.q@gmail.com>
2023-11-10 01:45:18 +0800

    1

---
+++ Assets/Game/_Script/AOT/Editor/RemoveMeshColliders.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class RemoveMeshColliders : MonoBehaviour
+{
+    [MenuItem("Tools/Remove Mesh Colliders from Prefabs")]
+    public static void RemoveCollidersFromPrefabs()
+    {
+        // 获取场景中的所有Prefab
+        GameObject[] prefabs = Resources.FindObjectsOfTypeAll<GameObject>();
+
+        foreach (GameObject prefab in prefabs)
+        {
+            // 检查Prefab Asset 是否以 ".prefab" 结尾，以排除场景中的实例
+            if (PrefabUtility.IsPartOfPrefabAsset(prefab))
+            {
+                // 检查Prefab是否包含MeshCollider组件
+                if (prefab.TryGetComponent<Rigidbody>(out var  ae))
+                {
+                    continue;
+                }
+                MeshCollider meshCollider = prefab.GetComponent<MeshCollider>();
+                if (meshCollider != null)
+                {
+                    // 移除MeshCollider组件
+                    DestroyImmediate(meshCollider, true);
+
+                    // 标记Prefab为已修改，以便保存
+                    PrefabUtility.RecordPrefabInstancePropertyModifications(prefab);
+
+                    Debug.Log("Removed MeshCollider from Prefab: " + prefab.name);
+                }
+            }
+        }
+
+        // 保存场景
+        EditorSceneManager.SaveOpenScenes();
+    }
+}
