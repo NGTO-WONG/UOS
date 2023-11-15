6d843f05f96192c800f2be6c1e2bdab982db6d72
huangqiang <2012wong.q@gmail.com>
2023-11-15 17:23:40 +0800

    1

--- Assets/Game/_Script/AOT/Editor/BuildPipeline_Build.cs
+++ Assets/Game/_Script/AOT/Editor/BuildPipeline_Build.cs
@@ -123,9 +123,11 @@ namespace Game._Script.AOT.Editor
         /// <param name="buildTarget"></param>
         public static void Compile(BuildTarget buildTarget)
         {
-            // PrebuildCommand.GenerateAll();
-            // AssetDatabase.Refresh();
-            // return;
+            PrebuildCommand.GenerateAll();
+            AssetDatabase.Refresh();
+            File.Copy(Path.Combine(Application.dataPath, "HybridCLRGenerate", "link.xml"),Path.Combine(Application.dataPath, "link.xml"),true);
+            
+            return;
             
             CompileDllCommand.CompileDll(buildTarget,BuildConfigAccessor.Instance.IsDevelopmentBuild.Equals("1"));
             Il2CppDefGeneratorCommand.GenerateIl2CppDef();
