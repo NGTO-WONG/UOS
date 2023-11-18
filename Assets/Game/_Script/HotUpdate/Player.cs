84bf6356df13e8f9c81f5e05d236a5ce3f7e8e25
huangqiang <2012wong.q@gmail.com>
2023-11-18 20:08:55 +0800

    1

--- Assets/Game/_Script/HotUpdate/Player.cs
+++ Assets/Game/_Script/HotUpdate/Player.cs
@@ -1,29 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    // private Rigidbody2D rigidbody2D;
+    private Rigidbody2D rigidbody2D;
     // [SerializeField] Rigidbody rigidbody;
     // private NavMeshAgent navMeshAgent;
-    //
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    //     rigidbody2D = GetComponent<Rigidbody2D>();
-    //     navMeshAgent = rigidbody.gameObject.GetComponent<NavMeshAgent>();
-    // }
-    //
-    // // Update is called once per frame
-    // void Update()
-    // {
-    //     if (Input.touchCount>0 || Input.GetMouseButtonDown(0))
-    //     {
-    //         rigidbody2D.AddForce(Vector2.up*200);
-    //         rigidbody.AddForce(Vector3.up*200);
-    //         navMeshAgent.SetDestination(Vector3.zero);
-    //     }
-    // }
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+       // navMeshAgent = rigidbody.gameObject.GetComponent<NavMeshAgent>();
+    }
+    
+    void Update()
+    {
+        if (Input.touchCount>0 || Input.GetMouseButtonDown(0))
+        {
+            rigidbody2D.AddForce(Vector2.up*200);
+            Debug.Log("A");
+           // rigidbody.AddForce(Vector3.up*200);
+            //navMeshAgent.SetDestination(Vector3.zero);
+        }
+    }
 }
