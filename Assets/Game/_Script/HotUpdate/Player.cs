78122cccdbfdee88f9c52f79b8dc1484f1a94958
huangqiang <2012wong.q@gmail.com>
2023-11-18 19:14:41 +0800

    1

---
+++ Assets/Game/_Script/HotUpdate/Player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logg : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount>0 || Input.GetMouseButtonDown(0))
+        {
+            rigidbody2D.AddForce(Vector2.up*200);
+        }
+    }
+}
